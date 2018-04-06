437399988e133d926e4ebdef775f3a56fcfe2db8
Ali Gholami <aligholami7596@gmail.com>
2018-04-07 00:41:20 +0430

    Defines the nearest neighbour

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from .knn import NearestNeighbour as NN
 
 # Data root path
 DATA_ROOT = './data/'
 
-class
+# Create an NN object
+clf = NN()
+
