2b334c382aa6fe3c222bdc3be8c8976e9fb1f44c
Ali Gholami <aligholami7596@gmail.com>
2018-04-07 11:40:03 +0430

    Added utilities

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+# ========================================
+# [] File Name : utils.py
+#
+# [] Creation Date : April 2018
+#
+# [] Created By : Ali Gholami (aligholami7596@gmail.com)
+# ========================================
+
+
+def load_cifar(path):
+    '''
+    Loads the CIFAR Image Dataset
+    :param path: string
+    :return: given dataset path in a numpy array
+    '''
+    pass
