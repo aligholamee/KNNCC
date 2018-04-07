c6adb8be9a1376ee992cbf193308431e19ea80b4
Ali Gholami <aligholami7596@gmail.com>
2018-04-07 13:06:40 +0430

    Now converting images to numpy array

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,22 @@
 # [] Created By : Ali Gholami (aligholami7596@gmail.com)
 # ========================================
 
+import glob
+import numpy as np
+from PIL import Image
 
 def load_cifar(path):
-    '''
+    """
     Loads the CIFAR Image Dataset
     :param path: string
     :return: given dataset path in a numpy array
-    '''
-    pass
+    """
+
+    # Get the image filelist
+    image_list = glob.glob(path)
+
+    # Load the images one by one
+    data = np.array([np.array(Image.open(image)) for image in image_list])
+
+    return data
+
