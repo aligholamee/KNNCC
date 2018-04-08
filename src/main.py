a52333e32f760cb0dbff98ed2b4b49ddff37ba7f
Ali Gholami <aligholami7596@gmail.com>
2018-04-08 16:32:44 +0430

    Loads the whole data and batches

--- src/main.py
+++ src/main.py
@@ -12,12 +12,19 @@ from sklearn.model_selection import train_test_split
 
 # Data root path
 DATA_ROOT = './data/'
+BATCH_LIST = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
 
 # Load the data
-data, labels = load_cifar(DATA_ROOT + 'data_batch_1')
+train_data = np.array([])
+train_labels = np.array([])
 
-print(data.shape)
-print(labels.shape)
+for batch in BATCH_LIST:
+    data, labels = load_cifar(DATA_ROOT + batch)
+    np.append(train_data, data)
+    np.append(train_labels, labels)
+
+print(train_data.shape)
+print(train_labels.shape)
 # Allocate 0.2 of the data as test data
 # train_data, test_data, train_labels, test_labels = train_test_split(data, random_state=True, test_size=0.2)
 
