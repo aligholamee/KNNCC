d2cb09002adc52af35805af4b685cb8f9d532661
Ali Gholami <aligholami7596@gmail.com>
2018-04-08 16:24:37 +0430

    Improve load cifar functionality

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from .knn import NearestNeighbour as NN
+# ========================================
+# [] File Name : main.py
+#
+# [] Creation Date : April 2018
+#
+# [] Created By : Ali Gholami (aligholami7596@gmail.com)
+# ========================================
+
+# from .knn import NearestNeighbour as NN
+from utils import load_cifar
+from sklearn.model_selection import train_test_split
 
 # Data root path
 DATA_ROOT = './data/'
 
-# Create an NN object
-clf = NN()
+# Load the data
+data, labels = load_cifar(DATA_ROOT + 'data_batch_1')
 
+print(data.shape)
+print(labels.shape)
+# Allocate 0.2 of the data as test data
+# train_data, test_data, train_labels, test_labels = train_test_split(data, random_state=True, test_size=0.2)
+
+# Create an NN object
+# clf = NN()
